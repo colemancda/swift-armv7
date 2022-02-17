8c9a1707550af1530fe67fa5a77a715ad964f4fe
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-02-17 03:38:47 -0800

    Apply armv7 patches

--- fetch-sources.sh
+++ fetch-sources.sh
@@ -1,7 +1,5 @@
 SWIFT_VERSION=5.5.3
 
-echo "Download Swift ${SWIFT_VERSION}"
-
 mkdir -p ./downloads
 mkdir -p ./build
 
@@ -11,6 +9,7 @@ SRCURL=https://github.com/apple/swift/archive/refs/tags/swift-${SWIFT_VERSION}-R
 if test -f "$DOWNLOAD_FILE"; then
     echo "$DOWNLOAD_FILE exists"
 else
+    echo "Download Swift ${SWIFT_VERSION}"
     wget $SRCURL -O $DOWNLOAD_FILE
 fi
 rm -rf $SRCDIR
@@ -18,7 +17,16 @@ mkdir -p $SRCDIR
 tar -xf $DOWNLOAD_FILE -C ./build
 
 # Apply patches
-
+echo "Apply Swift patches"
+DOWNLOAD_FILE=./downloads/swift-stdlib-refcount.diff
+SRCURL=https://github.com/uraimo/buildSwiftOnARM/raw/master/swift.diffs/aarch32/RefCount.h.diff
+if test -f "$DOWNLOAD_FILE"; then
+    echo "$DOWNLOAD_FILE exists"
+else
+    echo "Download ${DOWNLOAD_FILE}"
+    wget $SRCURL -O $DOWNLOAD_FILE
+fi
+patch ./build/swift-swift-${SWIFT_VERSION}-RELEASE/stdlib/public/SwiftShims/RefCount.h $DOWNLOAD_FILE 
 
 DOWNLOAD_FILE=./downloads/libdispatch-${SWIFT_VERSION}.tar.gz
 SRCDIR=./build/swift-corelibs-libdispatch-swift-${SWIFT_VERSION}-RELEASE
@@ -26,6 +34,7 @@ SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/refs/tags/swi
 if test -f "$DOWNLOAD_FILE"; then
     echo "$DOWNLOAD_FILE exists"
 else
+    echo "Download Dispatch ${SWIFT_VERSION}"
     wget $SRCURL -O $DOWNLOAD_FILE
 fi
 rm -rf $SRCDIR
@@ -38,6 +47,7 @@ SRCURL=https://github.com/apple/swift-corelibs-foundation/archive/refs/tags/swif
 if test -f "$DOWNLOAD_FILE"; then
     echo "$DOWNLOAD_FILE exists"
 else
+    echo "Download Foundation ${SWIFT_VERSION}"
     wget $SRCURL -O ./build
 fi
 rm -rf $SRCDIR
