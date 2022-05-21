d47b94a1eb8ef6fda902bd21a667ff8f4a7a949b
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-05-21 10:57:22 -0700

    Added build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+# Fetch and patch sources
+./fetch-sources.sh
+# Build runtime
+./build-swift-stdlib.sh
+./build-dispatch.sh
+./build-foundation.sh
+# Cross compile test package
+./generate-swiftpm-toolchain.sh
+./build-swift-hello.sh
+# Archive
+./build-tar.sh
