4216e3d6d3c6f6dc8be85d7972daf61df6b45fa5
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-05-21 10:32:42 -0700

    Added script for generating tar

---
+++ build-tar.sh
@@ -0,0 +1,22 @@
+# Configurable
+source .config
+source swift-define
+
+set -e
+
+# Build paths
+SWIFT_INSTALL_PREFIX=$SRC_ROOT/build/swift-armv7-install/usr
+LIBDISPATCH_INSTALL_PREFIX=$SRC_ROOT/build/libdispatch-armv7-install/usr
+FOUNDATION_INSTALL_PREFIX=$SRC_ROOT/build/foundation-armv7-install/usr
+INSTALL_PREFIX=$SRC_ROOT/build/swift-install
+INSTALL_TAR=$SRC_ROOT/build/swift-armv7.tar.gz
+
+# Combine build output
+rm -rf $INSTALL_PREFIX
+mkdir -p $INSTALL_PREFIX
+cp -rf $SWIFT_INSTALL_PREFIX/* $INSTALL_PREFIX/
+cp -rf $LIBDISPATCH_INSTALL_PREFIX/* $INSTALL_PREFIX/
+cp -rf $FOUNDATION_INSTALL_PREFIX/* $INSTALL_PREFIX/
+
+# compress
+tar -czvf $INSTALL_TAR $INSTALL_PREFIX
