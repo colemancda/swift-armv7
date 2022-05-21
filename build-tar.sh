bf28fe7ed44d95ad4df8e25f8de22d04a5460f06
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-05-21 10:38:45 -0700

    Fixed generating tar

--- build-tar.sh
+++ build-tar.sh
@@ -8,10 +8,11 @@ set -e
 SWIFT_INSTALL_PREFIX=$SRC_ROOT/build/swift-armv7-install/usr
 LIBDISPATCH_INSTALL_PREFIX=$SRC_ROOT/build/libdispatch-armv7-install/usr
 FOUNDATION_INSTALL_PREFIX=$SRC_ROOT/build/foundation-armv7-install/usr
-INSTALL_PREFIX=$SRC_ROOT/build/swift-install
+INSTALL_PREFIX=$SRC_ROOT/build/swift-install/usr
 INSTALL_TAR=$SRC_ROOT/build/swift-armv7.tar.gz
 
 # Combine build output
+rm -rf $INSTALL_TAR
 rm -rf $INSTALL_PREFIX
 mkdir -p $INSTALL_PREFIX
 cp -rf $SWIFT_INSTALL_PREFIX/* $INSTALL_PREFIX/
@@ -19,4 +20,5 @@ cp -rf $LIBDISPATCH_INSTALL_PREFIX/* $INSTALL_PREFIX/
 cp -rf $FOUNDATION_INSTALL_PREFIX/* $INSTALL_PREFIX/
 
 # compress
-tar -czvf $INSTALL_TAR $INSTALL_PREFIX
+cd $SRC_ROOT/build/swift-install
+tar -czvf $INSTALL_TAR .
