d47503cb99390f610b16f048d59a3ef52970a7c0
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-05-23 22:17:42 -0700

    Fixed Swift hello build script

--- build-swift-hello.sh
+++ build-swift-hello.sh
@@ -3,9 +3,10 @@ set -e
 source swift-define
 
 # Build paths
-SWIFT_HELLO_SRCDIR=$SRC_ROOT/swift-hello
-SWIFT_HELLO_BUILDDIR=$SRC_ROOT/build/swift-hello
+export SWIFT_PACKAGE_SRCDIR=$SRC_ROOT/swift-hello
+export SWIFT_PACKAGE_BUILDDIR=$SRC_ROOT/build/swift-hello
+mkdir -p $SWIFT_PACKAGE_BUILDDIR
 ./build-swift-package.sh
 
 echo "Copy swift-hello"
-cp $SWIFT_HELLO_BUILDDIR/${SWIFTPM_CONFIGURATION}/swift-hello ${STAGING_DIR}/usr/bin/swift-hello
+cp $SWIFT_PACKAGE_BUILDDIR/${SWIFTPM_CONFIGURATION}/swift-hello ${STAGING_DIR}/usr/bin/swift-hello
