a4d78c574cc1da1476761085b815b54fe3b90e12
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-05-26 14:13:05 -0700

    Allow specifying version tag

--- fetch-sources.sh
+++ fetch-sources.sh
@@ -7,8 +7,8 @@ mkdir -p ./build
 
 # Download Swift
 DOWNLOAD_FILE=./downloads/swift-${SWIFT_VERSION}.tar.gz
-SRCDIR=./downloads/swift-swift-${SWIFT_VERSION}-RELEASE
-SRCURL=https://github.com/apple/swift/archive/refs/tags/swift-${SWIFT_VERSION}-RELEASE.tar.gz
+SRCDIR=./downloads/swift-$SWIFT_VERSION
+SRCURL=https://github.com/apple/swift/archive/refs/tags/$SWIFT_VERSION.tar.gz
 if test -f "$DOWNLOAD_FILE"; then
     echo "$DOWNLOAD_FILE exists"
 else
@@ -29,7 +29,7 @@ else
     echo "Download ${DOWNLOAD_FILE}"
     wget $SRCURL -O $DOWNLOAD_FILE
 fi
-patch ./downloads/swift-swift-${SWIFT_VERSION}-RELEASE/stdlib/public/SwiftShims/RefCount.h $DOWNLOAD_FILE 
+patch ./downloads/swift-$SWIFT_VERSION/stdlib/public/SwiftShims/RefCount.h $DOWNLOAD_FILE 
 
 DOWNLOAD_FILE=./downloads/swift-stdlib-float16.patch
 SRCURL=https://gist.githubusercontent.com/colemancda/ed295f963493726b1c01a6bc97945a3d/raw/8392d963c96ee5c910b816734afca4295a7e09c7/Float16.patch
@@ -39,12 +39,12 @@ else
     echo "Download ${DOWNLOAD_FILE}"
     wget $SRCURL -O $DOWNLOAD_FILE
 fi
-patch ./downloads/swift-swift-${SWIFT_VERSION}-RELEASE/stdlib/public/runtime/Float16Support.cpp $DOWNLOAD_FILE 
+patch ./downloads/swift-$SWIFT_VERSION/stdlib/public/runtime/Float16Support.cpp $DOWNLOAD_FILE 
 
 # Download Dispatch
 DOWNLOAD_FILE=./downloads/libdispatch-${SWIFT_VERSION}.tar.gz
-SRCDIR=./downloads/swift-corelibs-libdispatch-swift-${SWIFT_VERSION}-RELEASE
-SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/refs/tags/swift-${SWIFT_VERSION}-RELEASE.tar.gz
+SRCDIR=./downloads/swift-corelibs-libdispatch-$SWIFT_VERSION
+SRCURL=https://github.com/apple/swift-corelibs-libdispatch/archive/refs/tags/$SWIFT_VERSION.tar.gz
 if test -f "$DOWNLOAD_FILE"; then
     echo "$DOWNLOAD_FILE exists"
 else
@@ -65,12 +65,12 @@ else
     echo "Download ${DOWNLOAD_FILE}"
     wget $SRCURL -O $DOWNLOAD_FILE
 fi
-patch ./downloads/swift-corelibs-libdispatch-swift-${SWIFT_VERSION}-RELEASE/src/shims/yield.c $DOWNLOAD_FILE 
+patch ./downloads/swift-corelibs-libdispatch-$SWIFT_VERSION/src/shims/yield.c $DOWNLOAD_FILE 
 
 # Download Foundation
 DOWNLOAD_FILE=./downloads/foundation-${SWIFT_VERSION}.tar.gz
-SRCDIR=./downloads/swift-corelibs-foundation-swift-${SWIFT_VERSION}-RELEASE
-SRCURL=https://github.com/apple/swift-corelibs-foundation/archive/refs/tags/swift-${SWIFT_VERSION}-RELEASE.tar.gz
+SRCDIR=./downloads/swift-corelibs-foundation-$SWIFT_VERSION
+SRCURL=https://github.com/apple/swift-corelibs-foundation/archive/refs/tags/$SWIFT_VERSION.tar.gz
 if test -f "$DOWNLOAD_FILE"; then
     echo "$DOWNLOAD_FILE exists"
 else
@@ -83,8 +83,8 @@ tar -xf $DOWNLOAD_FILE -C ./downloads
 
 # Download XCTest
 DOWNLOAD_FILE=./downloads/xctest-${SWIFT_VERSION}.tar.gz
-SRCDIR=./downloads/swift-corelibs-xctest-swift-${SWIFT_VERSION}-RELEASE
-SRCURL=https://github.com/apple/swift-corelibs-xctest/archive/refs/tags/swift-${SWIFT_VERSION}-RELEASE.tar.gz
+SRCDIR=./downloads/swift-corelibs-xctest-$SWIFT_VERSION
+SRCURL=https://github.com/apple/swift-corelibs-xctest/archive/refs/tags/$SWIFT_VERSION.tar.gz
 if test -f "$DOWNLOAD_FILE"; then
     echo "$DOWNLOAD_FILE exists"
 else
