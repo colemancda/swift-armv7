a4ad23785211b4eefca04ffece27167a8e3b9179
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-09-11 11:47:49 -0700

    Updated to Swift 5.7 development

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ Set the required environment variables for the scripts:
 export SRC_ROOT=/home/coleman/Developer/swift-armv7
 export STAGING_DIR="${STAGING_DIR:=$SRC_ROOT/bullseye-armv7}"
 export SWIFT_NATIVE_PATH="${SWIFT_NATIVE_PATH:=/usr/bin}"
-export SWIFT_LLVM_DIR="${SWIFT_LLVM_DIR:=/usr/lib/llvm-12}"
+export SWIFT_LLVM_DIR="${SWIFT_LLVM_DIR:=/usr/lib/llvm-15}"
 ```
 
-If using Debian or Ubuntu, install the `llvm-12` package for LLVM headers.
+If using Debian or Ubuntu, install the `llvm-15` package for LLVM headers.
 Make sure the Debian Bullseye armhf sysroot is at `STAGING_DIR` and `SWIFT_NATIVE_PATH` points to your Swift compiler installation.
 `SWIFT_PACKAGE_SRCDIR` can be set to the root of your own packages to cross compile them using `build-swift-package.sh`.
--- fetch-sources.sh
+++ fetch-sources.sh
@@ -19,18 +19,6 @@ rm -rf $SRCDIR
 mkdir -p $SRCDIR
 tar -xf $DOWNLOAD_FILE -C ./downloads
 
-# Apply Swift patches
-echo "Apply Swift patches"
-DOWNLOAD_FILE=./downloads/swift-stdlib-refcount.patch
-SRCURL=https://gist.githubusercontent.com/colemancda/ed295f963493726b1c01a6bc97945a3d/raw/8392d963c96ee5c910b816734afca4295a7e09c7/RefCount.patch
-if test -f "$DOWNLOAD_FILE"; then
-    echo "$DOWNLOAD_FILE exists"
-else
-    echo "Download ${DOWNLOAD_FILE}"
-    wget $SRCURL -O $DOWNLOAD_FILE
-fi
-patch ./downloads/swift-$SWIFT_VERSION/stdlib/public/SwiftShims/RefCount.h $DOWNLOAD_FILE 
-
 DOWNLOAD_FILE=./downloads/swift-stdlib-float16.patch
 SRCURL=https://gist.githubusercontent.com/colemancda/ed295f963493726b1c01a6bc97945a3d/raw/8392d963c96ee5c910b816734afca4295a7e09c7/Float16.patch
 if test -f "$DOWNLOAD_FILE"; then
@@ -40,26 +28,6 @@ else
     wget $SRCURL -O $DOWNLOAD_FILE
 fi
 patch ./downloads/swift-$SWIFT_VERSION/stdlib/public/runtime/Float16Support.cpp $DOWNLOAD_FILE
-
-DOWNLOAD_FILE=./downloads/swift-stdlib-AtomicWaitQueue.patch
-SRCURL=https://gist.githubusercontent.com/colemancda/d88a775f2bf3e234f4fa705c46b66b37/raw/1d8d54f23f5564f847ab39c65b59d287bba4f333/swift-5.6-AtomicWaitQueue.patch
-if test -f "$DOWNLOAD_FILE"; then
-    echo "$DOWNLOAD_FILE exists"
-else
-    echo "Download ${DOWNLOAD_FILE}"
-    wget $SRCURL -O $DOWNLOAD_FILE
-fi
-patch ./downloads/swift-$SWIFT_VERSION/include/swift/Runtime/AtomicWaitQueue.h $DOWNLOAD_FILE
-
-DOWNLOAD_FILE=./downloads/swift-stdlib-HeapObject.patch
-SRCURL=https://gist.github.com/colemancda/d88a775f2bf3e234f4fa705c46b66b37/raw/1d8d54f23f5564f847ab39c65b59d287bba4f333/swift-5.6-HeapObject-cxx-newObject.patch
-if test -f "$DOWNLOAD_FILE"; then
-    echo "$DOWNLOAD_FILE exists"
-else
-    echo "Download ${DOWNLOAD_FILE}"
-    wget $SRCURL -O $DOWNLOAD_FILE
-fi
-patch ./downloads/swift-$SWIFT_VERSION/include/swift/Runtime/HeapObject.h $DOWNLOAD_FILE
 
 # Download Dispatch
 DOWNLOAD_FILE=./downloads/libdispatch-${SWIFT_VERSION}.tar.gz
@@ -74,18 +42,6 @@ fi
 rm -rf $SRCDIR
 mkdir -p $SRCDIR
 tar -xf $DOWNLOAD_FILE -C ./downloads
-
-# Apply Dispatch patches
-echo "Apply Dispatch patches"
-DOWNLOAD_FILE=./downloads/libdispatch-yield.patch
-SRCURL=https://gist.githubusercontent.com/colemancda/a7c116250ceb601da5b408a4fd36f239/raw/7de75208472e5ce04954715ea5b59d7980ce57ff/arm-yield.patch
-if test -f "$DOWNLOAD_FILE"; then
-    echo "$DOWNLOAD_FILE exists"
-else
-    echo "Download ${DOWNLOAD_FILE}"
-    wget $SRCURL -O $DOWNLOAD_FILE
-fi
-patch ./downloads/swift-corelibs-libdispatch-$SWIFT_VERSION/src/shims/yield.c $DOWNLOAD_FILE 
 
 # Download Foundation
 DOWNLOAD_FILE=./downloads/foundation-${SWIFT_VERSION}.tar.gz
