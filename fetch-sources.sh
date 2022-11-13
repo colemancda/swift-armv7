d137f654a4fcd59ecc41987835f5b88c614aaad4
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-11-13 00:05:18 -0800

    Fixed patching sources

--- fetch-sources.sh
+++ fetch-sources.sh
@@ -17,12 +17,12 @@ else
     echo "Patch Swift"
     cd ./swift
     # git apply $SRC_ROOT/patches/swift/001-Float16.patch
-    patch $SWIFT_SRCDIR/stdlib/public/runtime/Float16Support.cpp $SRC_ROOT/patches/swift/patches/swift/001-swift-float16.patch
+    patch $SWIFT_SRCDIR/stdlib/public/runtime/Float16Support.cpp $SRC_ROOT/patches/swift/001-swift-float16.patch
     git apply $SRC_ROOT/patches/swift/002-swift-include-swift-AST-Expr.h.patch
     git apply $SRC_ROOT/patches/swift/003-swift-include-swift-Basic-BridgingUtils.h.patch
     git apply $SRC_ROOT/patches/swift/004-swift-targets.patch
     cd ../swiftpm
-    git apply $SRC_ROOT/patches/swiftpm/patches/swiftpm/001-swiftpm-bootstrap.patch
+    git apply $SRC_ROOT/patches/swiftpm/001-swiftpm-bootstrap.patch
     cd ../sourcekit-lsp
     git apply $SRC_ROOT/patches/sourcekit-lsp/001-sourcekit-lsp-build-script-helper.patch
 fi
