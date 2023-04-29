12c10b7f70590c2b8f6cb6e332b6cc4556ed995f
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-29 15:30:53 -0700

    Updated patches for Swift 5.8

--- fetch-sources.sh
+++ fetch-sources.sh
@@ -19,8 +19,7 @@ else
     # git apply $SRC_ROOT/patches/swift/001-Float16.patch
     patch $SWIFT_SRCDIR/stdlib/public/runtime/Float16Support.cpp $SRC_ROOT/patches/swift/001-swift-float16.patch
     git apply $SRC_ROOT/patches/swift/002-swift-include-swift-AST-Expr.h.patch
-    git apply $SRC_ROOT/patches/swift/003-swift-include-swift-Basic-BridgingUtils.h.patch
-    git apply $SRC_ROOT/patches/swift/004-swift-targets.patch
+    git apply $SRC_ROOT/patches/swift/003-swift-targets.patch
     cd ../swiftpm
     git apply $SRC_ROOT/patches/swiftpm/001-swiftpm-bootstrap.patch
     cd ../sourcekit-lsp
