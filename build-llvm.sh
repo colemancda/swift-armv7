4ca39fbc9c0f79d4ac885fc9fdc555b94b8b304b
Jesse L. Zamora <xtremekforever@gmail.com>
2024-03-16 14:28:18 -0400

    Move build to workspace/ dir, disable patches, update llvm build for 5.10

--- build-llvm.sh
+++ build-llvm.sh
@@ -2,9 +2,6 @@
 set -e
 source swift-define
 
-echo "Create LLVM build folder ${LLVM_BUILDDIR}"
-mkdir -p $LLVM_BUILDDIR
-
 echo "Build LLVM"
 cd $SWIFT_SRCDIR
 export SKIP_XCODE_VERSION_CHECK=1
@@ -13,12 +10,13 @@ export SWIFT_NATIVE_CLANG_TOOLS_PATH=$SWIFT_NATIVE_PATH
 export CC=$SWIFT_NATIVE_PATH/clang
 export CFLAGS="-fPIC"
 
-./utils/build-script -RA --build-swift-tools=0 \
-    --skip-early-swift-driver --skip-build-llvm --skip-build-cmark  \
+./utils/build-script -RA --build-swift-tools=0 --swift-enable-backtracing=0 \
+    --swift-threading-package=c11 --swift-stdlib-supports-backtrace-reporting=0 \
+    --skip-early-swift-driver --skip-early-swiftsyntax \
+    --skip-build-llvm --skip-build-cmark --skip-build-swift \
     --native-swift-tools-path=$SWIFT_NATIVE_PATH \
     --native-clang-tools-path=$SWIFT_NATIVE_PATH \
     --swift-install-components='clang-resource-dir-symlink;license;stdlib;sdk-overlay' \
-    --install-swift \
+    --install-swift --install-swiftsyntax \
     --cross-compile-append-host-target-to-destdir=False --build-swift-dynamic-stdlib \
-    --build-dir=$SRC_ROOT/build \
-    --workspace=$SRC_ROOT/downloads \
+    --workspace=$WORKSPACE_DIR
--- fetch-sources.sh
+++ fetch-sources.sh
@@ -2,11 +2,10 @@
 set -e
 source swift-define
 
-mkdir -p ./downloads
-mkdir -p ./build
+mkdir -p $WORKSPACE_DIR
 
 # Fetch sources
-cd ./downloads
+cd $WORKSPACE_DIR
 if [[ -d "$SWIFT_SRCDIR" ]]; then
     echo "$SWIFT_SRCDIR exists"
 else
@@ -17,12 +16,12 @@ else
     echo "Patch Swift"
     cd ./swift
     # git apply $SRC_ROOT/patches/swift/001-Float16.patch
-    patch $SWIFT_SRCDIR/stdlib/public/runtime/Float16Support.cpp $SRC_ROOT/patches/swift/001-swift-float16.patch
-    git apply $SRC_ROOT/patches/swift/002-swift-include-swift-AST-Expr.h.patch
-    git apply $SRC_ROOT/patches/swift/003-swift-include-swift-Basic-BridgingUtils.h.patch
-    git apply $SRC_ROOT/patches/swift/004-swift-targets.patch
-    cd ../swiftpm
-    git apply $SRC_ROOT/patches/swiftpm/001-swiftpm-bootstrap.patch
-    cd ../sourcekit-lsp
-    git apply $SRC_ROOT/patches/sourcekit-lsp/001-sourcekit-lsp-build-script-helper.patch
+    #patch $SWIFT_SRCDIR/stdlib/public/runtime/Float16Support.cpp $SRC_ROOT/patches/swift/001-swift-float16.patch
+    #git apply $SRC_ROOT/patches/swift/002-swift-include-swift-AST-Expr.h.patch
+    #git apply $SRC_ROOT/patches/swift/003-swift-include-swift-Basic-BridgingUtils.h.patch
+    #git apply $SRC_ROOT/patches/swift/004-swift-targets.patch
+    #cd ../swiftpm
+    #git apply $SRC_ROOT/patches/swiftpm/001-swiftpm-bootstrap.patch
+    #cd ../sourcekit-lsp
+    #git apply $SRC_ROOT/patches/sourcekit-lsp/001-sourcekit-lsp-build-script-helper.patch
 fi
