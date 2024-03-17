d3f5f3937dacc1d322db100a7be138f96a890ff8
Jesse L. Zamora <xtremekforever@gmail.com>
2024-03-17 08:18:23 -0400

    Update swift-hello example

--- build-swift-hello.sh
+++ build-swift-hello.sh
@@ -4,6 +4,6 @@ source swift-define
 
 # Build paths
 export SWIFT_PACKAGE_SRCDIR=$SRC_ROOT/swift-hello
-export SWIFT_PACKAGE_BUILDDIR=$SRC_ROOT/build/swift-hello
+export SWIFT_PACKAGE_BUILDDIR=$WORKSPACE_DIR/build/swift-hello
 mkdir -p $SWIFT_PACKAGE_BUILDDIR
 ./build-swift-package.sh
--- swift-hello/Sources/swift-hello/Hello.swift
+++ swift-hello/Sources/swift-hello/Hello.swift
@@ -1,113 +1,31 @@
-#if canImport(Dispatch)
-import Dispatch
-#endif
-#if canImport(Foundation)
 import Foundation
-#endif
-#if canImport(FoundationNetworking)
-import FoundationNetworking
-#endif
+import Dispatch
 
 @main
 struct Hello {
-
     static func main() async throws {
-        print("Hello, world! 👋")
-        try await testConcurrency()
-        #if canImport(Dispatch)
-        print("Dispatch installed")
-        testDispatch()
+        #if swift(>=5.10)
+            print("Hello from Swift 5.10! 👋")
+        #else
+            print("Hello from Swift older than 5.10....")
         #endif
-        #if canImport(Foundation)
-        print("Foundation installed")
-        testFoundation()
-        #endif
-        #if canImport(FoundationNetworking) || canImport(Darwin)
-        print("FoundationNetworking installed")
-        try await testFoundationNetworking()
-        #endif
-    }
-}
-
-struct TestError: Error {
-
-    let reason: String
-}
-
-func testConcurrency() async throws {
-    let task = Task {
-        var didCatchError = false
-        do { try await asyncErrorTest() }
-        catch CocoaError.userCancelled { didCatchError = true }
-        catch { fatalError("Unexpected error catching") }
-        precondition(didCatchError)
-    }
-    await task.value
-    for _ in 0 ..< 5 {
-        try await Task.sleep(nanoseconds: 100_000)
-        try await Task.sleep(for: .microseconds(10))
-    }
-    print("Concurrency test passed")
-}
-
-func asyncErrorTest(error: Error = CocoaError(.userCancelled)) async throws {
-    throw error
-}
-
-#if canImport(Foundation)
-func testFoundation() {
-    print("UUID:", UUID())
-    let formatter = DateFormatter()
-    formatter.dateStyle = .full
-    formatter.timeStyle = .full
-    print("Date:", formatter.string(from: Date()))
-}
-#endif
-
-#if canImport(Dispatch)
-func testDispatch() {
-    DispatchQueue.global().sync {
-        print("Dispatch thread: \(Thread.current.name ?? "\(Thread.current)")")
-    }
-}
-#endif
-
-#if canImport(FoundationNetworking) || canImport(Darwin)
-func testFoundationNetworking() async throws {
-    let url = URL(string: "https://httpbin.org/anything")!
-    let body = Data("test-\(UUID())".utf8)
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    request.httpBody = body
-    let (data, urlResponse) = try await URLSession.shared.data(for: request)
-    guard data.contains(body) else {
-        throw TestError(reason: "Invalid response")
-    }
-    guard let httpResponse = urlResponse as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-        throw TestError(reason: "Invalid response")
-    }
-    guard let json = String(data: data, encoding: .utf8) else {
-        throw TestError(reason: "Invalid response")
-    }
-    print(json)
-    print("URLSession test passed")
-}
-
-#if os(Linux)
-extension URLSession {
-
-    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
-        try await withCheckedThrowingContinuation { continuation in
-            let task = self.dataTask(with: request) { data, response, error in
-                if let error = error {
-                    continuation.resume(throwing: error)
-                } else {
-                    continuation.resume(returning: (data ?? .init(), response!))
-                }
-            }
-            task.resume()
+        let task = Task {
+            var didCatchError = false
+            do { try await errorTest() }
+            catch TestError.error { didCatchError = true }
+            catch { fatalError() }
+            print("Task ran")
         }
+        try await Task.sleep(nanoseconds: 1_000_000_000)
+        print("Bye")
     }
 }
-#endif
-#endif
+
+func errorTest() async throws {
+    print("Will throw")
+    throw TestError.error
+}
+
+enum TestError: Error {
+    case error
+}
