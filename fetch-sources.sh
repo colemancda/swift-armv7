abeddc78c93c660890ab627fa29a1a7ab1566a72
Jesse L. Zamora <xtremekforever@gmail.com>
2024-04-05 04:43:25 -0400

    Fix applying patches

--- fetch-sources.sh
+++ fetch-sources.sh
@@ -13,16 +13,4 @@ else
     echo "Checkout Swift"
     git clone https://github.com/apple/swift.git
     ./swift/utils/update-checkout --clone --tag $SWIFT_VERSION
-    # Patch Swift
-    echo "Patch Swift"
-    cd ./swift
-    # git apply $SRC_ROOT/patches/swift/001-Float16.patch
-    patch $SWIFT_SRCDIR/stdlib/public/runtime/Float16Support.cpp $SRC_ROOT/patches/swift/001-swift-float16.patch
-    git apply $SRC_ROOT/patches/swift/002-swift-include-swift-AST-Expr.h.patch
-    git apply $SRC_ROOT/patches/swift/003-swift-include-swift-Basic-BridgingUtils.h.patch
-    git apply $SRC_ROOT/patches/swift/004-swift-targets.patch
-    cd ../swiftpm
-    git apply $SRC_ROOT/patches/swiftpm/001-swiftpm-bootstrap.patch
-    cd ../sourcekit-lsp
-    git apply $SRC_ROOT/patches/sourcekit-lsp/001-sourcekit-lsp-build-script-helper.patch
 fi
