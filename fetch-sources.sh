a0ec3eb18fea18f0fce49c84f6fd23dacd6e2839
Jesse L. Zamora <xtremekforever@gmail.com>
2024-05-06 16:52:54 -0400

    Apply patch for cxx interop to Swift sources after checkout

--- fetch-sources.sh
+++ fetch-sources.sh
@@ -13,4 +13,8 @@ else
     echo "Checkout Swift"
     git clone https://github.com/apple/swift.git
     ./swift/utils/update-checkout --clone --tag $SWIFT_VERSION
+
+    # Apply patches
+    echo "Apply CXX interop patch"
+    patch -d $SWIFT_SRCDIR -p1 <$SRC_ROOT/patches/0001-Swift-fix-find-libstdc++-for-cxx-interop.patch
 fi
